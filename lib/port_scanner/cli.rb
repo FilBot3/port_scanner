1c30b96205576567b99a08b2a48a814438628221
Phillip Dudley <Predatorian3@gmail.com>
2015-10-16 15:47:08 -0500

    Corrected newline escapes in Thor long_desc

--- lib/port_scanner/cli.rb
+++ lib/port_scanner/cli.rb
@@ -6,20 +6,17 @@ module PortScanner
 
     desc "scan IPADDRESS PORT", "Two args: IP Address to scan, Port to check."
     long_desc <<-PORT_SCANNER 
-    This program, or RubyGem will perform a TCP connection check against a specified IP and Port.   
+    This program, or RubyGem will perform a TCP connection check against a specified IP and Port. \x5    
     
+    If the connection can be made, the output will be true. \x5
+    If the connection cannot be made, the output will be false. \x5  
 
-    If the connection can be made, the output will be true.   
-    
-    
-    If the connection cannot be made, the output will be false.  
+    EXAMPLES: \x5
+    $> port_scanner scan 192.169.1.2 22 \x5
+    true # If SSH is running \x5
 
-    EXAMPLES:   
-    $> port_scanner scan 192.169.1.2 22
-    true # If SSH is running
-
-    $> port_scanner scan 192.169.1.2 12345 # an unknown service? 
-    false
+    $> port_scanner scan 192.169.1.2 12345 # an unknown service?  \x5
+    false \x5
 
     PORT_SCANNER
     def scan( ipaddress, port )
